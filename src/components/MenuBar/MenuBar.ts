3e67b74cf7a96d9e26b232f42c705c4e22f05eff
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-02-11 15:41:54 +0100

    styling menu bar

--- src/components/MenuBar/MenuBar.ts
+++ src/components/MenuBar/MenuBar.ts
@@ -1,10 +1,11 @@
 import { defineComponent } from "vue";
 import logo from "../../assets/logo.svg";
+import triangle from "../../assets/triangle.svg"
 
 export default defineComponent({
   name: "menu-component",
 
   setup() {
-    return { logo };
+    return { logo, triangle };
   },
 });
--- src/components/MenuBar/MenuBar.vue
+++ src/components/MenuBar/MenuBar.vue
@@ -1,23 +1,32 @@
 <script src="./MenuBar.ts"></script>
 
 <template>
-  <header class="bg-blue-300">
+  <header class="bg-black">
     <div class="h-20 flex justify-center items-center">
       <figure>
         <img class="logo" :src="logo" alt="logo" />
       </figure>
     </div>
-    <nav class=" border border-gray">
-      <div class="bg-blue-600 flex max-w-7xl m-auto justify-between">
+    <nav class="border border-x-transparent border-stone-900">
+      <div class="bg-black flex max-w-7xl m-auto justify-between">
         <ul class="flex">
-          <RouterLink class="menu-item-lg" to="/statistics">HOME</RouterLink>
-          <RouterLink class="menu-item-lg" to="/statistics"
-            >STATISTICS</RouterLink
-          >
-          <RouterLink class="menu-item-lg" to="/statistics">TEAMS</RouterLink>
+          <RouterLink class="menu-item-lg border-l" to="/statistics">
+            HOME
+          </RouterLink>
+          <RouterLink class="menu-item-lg" to="/statistics">
+            STATISTICS
+            <img class="absolute bottom-0.5 right-0.5" :src="triangle" alt="" />
+          </RouterLink>
+          <RouterLink class="menu-item-lg" to="/statistics">
+            TEAMS
+            <img class="absolute bottom-0.5 right-0.5" :src="triangle" alt="" />
+          </RouterLink>
         </ul>
         <ul class="flex">
-          <RouterLink class="menu-item-lg" to="/statistics">ACCOUNT</RouterLink>
+          <RouterLink class="menu-item-lg border-l" to="/statistics">
+            ACCOUNT
+            <img class="absolute bottom-0.5 right-0.5" :src="triangle" alt="" />
+          </RouterLink>
         </ul>
       </div>
     </nav>
